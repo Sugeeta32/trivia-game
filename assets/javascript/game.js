1c060472f1b9d554b50de502a3370753e1a05329
Sugeeta32 <sugeeta.gupta@gmail.com>
2019-09-21 00:56:02 -0400

    Function to show the questions and options

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,34 @@
-console.log($);
+let counter = 30;
+let currentQuestion = 1;
+let loss = 0;
+let win = 0;
+let timer;
 
+//  Function to show the question and options
+
+function showQuestion() {
+
+	const question = questions[currentQuestion].ques;
+	const options = questions[currentQuestion].options;
+	
+	$("#timer").html("Timer :" + counter);
+	$("#game").html(`<h3>  ${question} </h3>
+
+	${showOptions(options)}
+	`);
+}
+
+showQuestion();
+
+
+function showOptions(options) {
+	
+	var result = " ";
+	for (var i =0; i < options.length; i++){
+		result += `<h4 class ="option" dataAnswer="${options[i]}">${options[i]}</h4>`;
+
+	}
+	
+return result;
+
+}
--- assets/javascript/question.js
+++ assets/javascript/question.js
@@ -1,21 +1,21 @@
-alert("this too is working");
-var question = [
+
+var questions = [
     {
-        ques: "what is my name?",
+        ques: "What is my name?",
         options: ["Sue", "Miranda", "Gargi", "Vaishnavi"],
         answwer: "Sue"
 
     },
 
     {
-        ques: "what is my education?",
+        ques: "What is my education?",
         options: ["BA", "MA", "CA", "MBA"],
         answwer: "MBA"
 
     },
 
     {
-        ques: "what is my age?",
+        ques: "What is my age?",
         options: ["32", "22", "42", "52"],
         answwer: "42"
 
