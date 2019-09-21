6d6adb484c2b9dd39737242deaa8f5bc27a8e158
Sugeeta32 <sugeeta.gupta@gmail.com>
2019-09-21 19:22:44 -0400

    Timer added and not going in negative

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,26 @@
-let counter = 30;
-let currentQuestion = 1;
+let counter = 3;
+let currentQuestion = 0;
 let loss = 0;
 let win = 0;
 let timer;
 
+// function to start and reset the timer for user 
+
+function clock(){
+	counter--;
+	$("#timer").html("Timer :" + counter);
+// to stop the timer at 0 and not going in negative
+	if (counter === 0){
+		clearInterval(timer);
+	}
+}
+
+
 //  Function to show the question and options
 
 function showQuestion() {
+	counter = 3;
+	timer = setInterval(clock, 1000);
 
 	const question = questions[currentQuestion].ques;
 	const options = questions[currentQuestion].options;
@@ -14,21 +28,21 @@ function showQuestion() {
 	$("#timer").html("Timer :" + counter);
 	$("#game").html(`<h3>  ${question} </h3>
 
-	${showOptions(options)}
+	 ${showOptions(options)}
 	`);
 }
 
 showQuestion();
 
-
+// function to show the options using the for loop for 4 displayed options, which is in question.js
 function showOptions(options) {
 	
 	var result = " ";
 	for (var i =0; i < options.length; i++){
-		result += `<h4 class ="option" dataAnswer="${options[i]}">${options[i]}</h4>`;
+		result += `<p class ="option" dataAnswer="${options[i]}">${options[i]}</p>`;
 
 	}
 	
 return result;
-
+ 
 }
