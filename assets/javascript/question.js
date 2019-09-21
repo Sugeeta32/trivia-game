abc1bcc472fc88b1a8d98bcfbfb8abbefa8b9162
Sugeeta32 <sugeeta.gupta@gmail.com>
2019-09-20 23:48:32 -0400

    Basic layout

---
+++ assets/javascript/question.js
@@ -0,0 +1,23 @@
+alert("this too is working");
+var question = [
+    {
+        ques: "what is my name?",
+        options: ["Sue", "Miranda", "Gargi", "Vaishnavi"],
+        answwer: "Sue"
+
+    },
+
+    {
+        ques: "what is my education?",
+        options: ["BA", "MA", "CA", "MBA"],
+        answwer: "MBA"
+
+    },
+
+    {
+        ques: "what is my age?",
+        options: ["32", "22", "42", "52"],
+        answwer: "42"
+
+    }
+];
