aa08d4f96f7386a8400bb25da07c4fd0ff944335
Sugeeta32 <sugeeta.gupta@gmail.com>
2019-09-22 09:43:10 -0400

    all questions and options are popping

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,56 @@
-let counter = 3;
+let counter = 10;
 let currentQuestion = 0;
 let loss = 0;
 let win = 0;
 let timer;
+// function to go the next question and call this function after the player has lost or answered correctly
+
+function nextQuestion() {
+	const isQuestionOver = (questions.length - 1) === currentQuestion;
+	if (isQuestionOver) {
+		alert("Game is over");
+		clearInterval(timer);
+		// timeUp();
+	} else {
+		currentQuestion++;
+		showQuestion();
+	}
+
+}
+
+// function time up
+function timeUp(){
+	clearInterval(timer);
+	loss++;
+	nextQuestion();
+}
 
 // function to start and reset the timer for user 
 
-function clock(){
+function clock() {
 	counter--;
 	$("#timer").html("Timer :" + counter);
-// to stop the timer at 0 and not going in negative
-	if (counter === 0){
-		clearInterval(timer);
+	// to stop the timer at 0 and not going in negative
+	if (counter === 0) {
+		 timeUp();
+		// clearInterval(timer);
+		// loss++;
+		// nextQuestion();
 	}
+
+
 }
 
 
 //  Function to show the question and options
 
 function showQuestion() {
-	counter = 3;
+	counter = 10;
 	timer = setInterval(clock, 1000);
 
 	const question = questions[currentQuestion].ques;
 	const options = questions[currentQuestion].options;
-	
+
 	$("#timer").html("Timer :" + counter);
 	$("#game").html(`<h3>  ${question} </h3>
 
@@ -36,13 +62,33 @@ showQuestion();
 
 // function to show the options using the for loop for 4 displayed options, which is in question.js
 function showOptions(options) {
-	
+
 	var result = " ";
-	for (var i =0; i < options.length; i++){
+	for (var i = 0; i < options.length; i++) {
 		result += `<p class ="option" dataAnswer="${options[i]}">${options[i]}</p>`;
 
 	}
-	
-return result;
- 
+
+	return result;
+
 }
+
+
+$(document).on ("click", ".option", function () {
+	clearInterval(timer);
+	const selectedOption =$(this).attr("dataAnswer");
+	const answer = questions[currentQuestion].answer;
+	
+	if(answer === selectedOption){
+		win++;
+		nextQuestion();
+		console.log("win");
+	} else{
+		loss++;
+		nextQuestion();
+		console.log("lost")
+
+	}
+
+});
+showQuestion();
--- assets/javascript/question.js
+++ assets/javascript/question.js
@@ -3,21 +3,21 @@ var questions = [
     {
         ques: "What is my name?",
         options: ["Sue", "Miranda", "Gargi", "Vaishnavi"],
-        answwer: "Sue"
+        answer: "Sue"
 
     },
 
     {
         ques: "What is my education?",
         options: ["BA", "MA", "CA", "MBA"],
-        answwer: "MBA"
+        answer: "MBA"
 
     },
 
     {
         ques: "What is my age?",
         options: ["32", "22", "42", "52"],
-        answwer: "42"
+        answer: "42"
 
     }
 ];
