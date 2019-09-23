c2549cb4b52492eef8ac0a59437082fe3ec6222c
Sugeeta32 <sugeeta.gupta@gmail.com>
2019-09-23 00:50:35 -0400

    Added remaining question line, reset button and new questions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,48 @@
-let counter = 10;
-let currentQuestion = 0;
+let counter = 20;
+let currentQuestionIndex = 0;
 let loss = 0;
 let win = 0;
 let timer;
 // function to go the next question and call this function after the player has lost or answered correctly
+// debugger;
+$("#reset").hide();
+showQuestion();
+
+//  Function to show the question and options
+
+function showQuestion() {
+	counter = 20;
+	timer = setInterval(clock, 1000);
+
+	const question = questions[currentQuestionIndex].ques;
+	const options = questions[currentQuestionIndex].options;
+
+	$("#timer").html("Timer :" + counter);
+	$("#game").html(`<h3>  ${question} </h3>${showOptions(options)} 
+	${questionRemainingLineForUser()}`);
+	
+}
 
 function nextQuestion() {
-	const isQuestionOver = (questions.length - 1) === currentQuestion;
+	const isQuestionOver = (questions.length - 1) === currentQuestionIndex;
 	if (isQuestionOver) {
 		alert("Game is over");
-		clearInterval(timer);
-		// timeUp();
+
+		displayResult();
+		reset();
+		//clock();
 	} else {
-		currentQuestion++;
+		currentQuestionIndex++;
 		showQuestion();
 	}
-
 }
 
 // function time up
-function timeUp(){
+function timeUp() {
 	clearInterval(timer);
-	loss++;
+
+	// loss++;
+
 	nextQuestion();
 }
 
@@ -32,7 +53,7 @@ function clock() {
 	$("#timer").html("Timer :" + counter);
 	// to stop the timer at 0 and not going in negative
 	if (counter === 0) {
-		 timeUp();
+		timeUp();
 		// clearInterval(timer);
 		// loss++;
 		// nextQuestion();
@@ -40,25 +61,6 @@ function clock() {
 
 
 }
-
-
-//  Function to show the question and options
-
-function showQuestion() {
-	counter = 10;
-	timer = setInterval(clock, 1000);
-
-	const question = questions[currentQuestion].ques;
-	const options = questions[currentQuestion].options;
-
-	$("#timer").html("Timer :" + counter);
-	$("#game").html(`<h3>  ${question} </h3>
-
-	 ${showOptions(options)}
-	`);
-}
-
-showQuestion();
 
 // function to show the options using the for loop for 4 displayed options, which is in question.js
 function showOptions(options) {
@@ -68,27 +70,59 @@ function showOptions(options) {
 		result += `<p class ="option" dataAnswer="${options[i]}">${options[i]}</p>`;
 
 	}
-
 	return result;
-
 }
 
 
-$(document).on ("click", ".option", function () {
+$(document).on("click", ".option", function () {
+
 	clearInterval(timer);
-	const selectedOption =$(this).attr("dataAnswer");
-	const answer = questions[currentQuestion].answer;
-	
-	if(answer === selectedOption){
+
+	const selectedOption = $(this).attr("dataAnswer");
+	const answer = questions[currentQuestionIndex].answer;
+
+	if (answer === selectedOption) {
 		win++;
+		// $("#wins").html("Wins:  " + win);
 		nextQuestion();
 		console.log("win");
-	} else{
+	} else {
 		loss++;
+		// $("#loss").html("Loss:  " + loss);$("#loss").html("Loss:  " + loss);
 		nextQuestion();
 		console.log("lost")
 
 	}
 
 });
-showQuestion();
+
+function displayResult() {
+	$("#reset").show();
+	$("#wins").html("Your score is  " + win + "" + " out of " + questions.length);
+	 $("#loss").html("Loss:  " + loss);
+	 
+};
+
+
+function reset() {
+	 
+	var reset = `<btn class="btn btn-primary" id ="reset">Let's Play again</btn>`
+	$("#reset").html(reset);
+	$("#reset").on("click", function () {
+
+		counter = 20;
+		loss = 0;
+		win = 0;
+		timer = null;
+		currentQuestionIndex = 0;
+		showQuestion();
+	});
+
+}
+function questionRemainingLineForUser(){
+	const questionsRemaining = questions.length-(currentQuestionIndex+1);
+	const totalQuestion = questions.length;
+
+	return `"Remaining questions: ${questionsRemaining}/${totalQuestion}`;
+
+}
--- assets/javascript/question.js
+++ assets/javascript/question.js
@@ -1,23 +1,109 @@
 
 var questions = [
     {
-        ques: "What is my name?",
-        options: ["Sue", "Miranda", "Gargi", "Vaishnavi"],
-        answer: "Sue"
+        ques: "What is the world’s second tallest mountain?",
+        options: ["Aconcagua in Argentina",
+            "Mount McKinley in Alaska",
+            "Kilimanjaro in Japan",
+            "Mount Logan in Canada"],
+        answer: "Aconcagua in Argentina"
 
     },
 
     {
-        ques: "What is my education?",
-        options: ["BA", "MA", "CA", "MBA"],
-        answer: "MBA"
+        ques: "What is the world’s largest shark?",
+        options: ["Basking Shark",
+            "Great White",
+            "Whale Shark",
+            "Pacific Sleeper Shark"],
+        answer: "Whale Shark"
 
     },
 
     {
-        ques: "What is my age?",
-        options: ["32", "22", "42", "52"],
-        answer: "42"
+        ques: "What actress helped develop the radio controlled missile? ",
+        options: ["Veronica Lake",
+            "Hedy Lamarr",
+            "Marilyn Monroe",
+            "Elizabeth Taylor"],
+        answer: "Hedy Lamarr"
 
-    }
-];
+    },
+
+    {
+        ques: "Why did pirates really have a patch over their eyes?",
+        options: ["To intimidate their enemies",
+        "To improve eyesight when looking long distance through a telescope",
+        "To cover an eye injury sustained during battle",
+        "To keep one eye adjusted to the dark for going below deck"
+    ],
+        answer: "To keep one eye adjusted to the dark for going below deck"
+
+    },
+    
+    {
+        ques: "What year did the Berlin Wall come down? ",
+        options: ["1985",
+        "1989",
+        "1987",
+        "1991"],
+        answer: "1989"
+
+    },
+
+    {
+        ques: "Who invented hot cocoa?",
+        options: [ "The Germans",
+        "The Incans",
+        "The Swiss",
+        "The Mayans"],
+        answer: "The Mayans"
+
+    },
+
+    {
+        ques: "In the human body, where is the smallest bone? ",
+        options: ["The toes",
+        "The nose",
+        "The neck",
+        "The ear" ],
+        answer: "The ear"
+
+    },
+
+
+    {
+        ques: "What Atari-made video game was considered one of the worst video games of all time?",
+        options: ["The Empire Strikes Back",
+        "Superman",
+        "E.T.",
+        "Towering Inferno" ],
+        answer:"E.T.", 
+
+    },
+
+    {
+        ques: "What’s the Largest Planet in the Solar System?",
+        options: [ "Saturn",
+        "Neptune",
+        "Jupiter",
+        "Uranus"],
+        answer:"Jupiter"
+
+    },
+
+    
+    {
+        ques: "What’s the fastest bird on the planet?",
+        options: [ "The bald eagle",
+        "The peregrine falcon",
+        "The hummingbird",
+        "The common sparrow"],
+        answer: "The peregrine falcon"
+
+    },
+
+
+]
+
+
